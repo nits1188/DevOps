60910762030aa4e2f79846cf7549ced073477c84
PDC4-Training.PDC4 <PDC4-Training.PDC4@P4A-508SD66.dir.svc.accenture.com>
2018-08-01 11:40:14 +0530

    EntirePorject

--- cucumber-project-skeleton/src/test/java/skeleton/MyProject.java
+++ cucumber-project-skeleton/src/test/java/skeleton/MyProject.java
@@ -1,13 +1,13 @@
 package skeleton;
 
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import junit.framework.Assert;
+
 
 public class MyProject {
 private WebDriver driver;
